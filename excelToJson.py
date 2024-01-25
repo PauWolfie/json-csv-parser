1efd8f970dbc443a60be15f5320bcb0b383c3ad9
Pau Llobet <pllobet@mychefpro.com>
2024-01-25 15:57:23 +0100

    Feat: code optimization. Feat: test json file.

--- excelToJson.py
+++ excelToJson.py
@@ -2,32 +2,32 @@ import os
 import pandas as pd
 import json
 
-# Nombre de la carpeta que contiene los archivos Excel
-carpeta_csv = 'csv'
+# Name of the folder containing the Excel files
+csv_folder = 'excel'
 
-# Nombre de la carpeta donde se guardarán los archivos JSON
-carpeta_json = 'json'
+# Name of the folder where the JSON files will be saved
+json_folder = 'json'
 
-# Asegurarse de que la carpeta de salida exista
-if not os.path.exists(carpeta_json):
-    os.makedirs(carpeta_json)
+# Ensure that the output folder exists
+if not os.path.exists(json_folder):
+    os.makedirs(json_folder)
 
-# Iterar sobre los archivos en la carpeta 'csv'
-for archivo_csv in os.listdir(carpeta_csv):
-    if archivo_csv.endswith('.xlsx'):
-        ruta_csv = os.path.join(carpeta_csv, archivo_csv)
+# Iterate over the files in the 'csv' folder
+for csv_file in os.listdir(csv_folder):
+    if csv_file.endswith('.xlsx'):
+        csv_path = os.path.join(csv_folder, csv_file)
 
-        # Leer el archivo Excel
-        df = pd.read_excel(ruta_csv)
+        # Read the Excel file
+        df = pd.read_excel(csv_path)
 
-        # Convertir el DataFrame a un diccionario personalizado
-        data_dict = dict(zip(df['Nombre_Columna_Indice'], df['Valor']))
+        # Convert the DataFrame to a custom dictionary
+        data_dict = dict(zip(df['Key'], df['Value']))
 
-        # Construir la ruta para el archivo JSON de salida en la carpeta 'json'
-        ruta_json = os.path.join(carpeta_json, os.path.splitext(archivo_csv)[0] + '.json')
+        # Build the path for the output JSON file in the 'json' folder
+        json_path = os.path.join(json_folder, os.path.splitext(csv_file)[0] + '.json')
 
-        # Escribir el diccionario a un archivo JSON
-        with open(ruta_json, 'w', encoding='utf-8') as json_file:
+        # Write the dictionary to a JSON file
+        with open(json_path, 'w', encoding='utf-8') as json_file:
             json.dump(data_dict, json_file, indent=2, ensure_ascii=False)
 
-print("La conversión de Excel a JSON ha sido completada.")
+print("Excel to JSON conversion has been completed.")
